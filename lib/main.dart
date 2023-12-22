2e27913765fa0b7f2a861aae619833f0fa323fa3
Mohammad Hisham <mzoom26@hotmail.com>
2023-12-22 05:47:21 +0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'dart:io';
+
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:videos_adaptive_app/src/adaptive_playlist.dart';
+
+import 'src/app_state.dart';
+import 'src/playlist_details.dart';
+import 'src/playlists.dart';
+
+// From https://www.youtube.com/channel/UCwXdFgeE9KYzlDdR7TG9cMw
+const flutterDevAccountId = 'UCgDWEoG_vWbSIjA9FEnrEAg';
+
+// TODO: Replace with your YouTube API Key
+const youTubeApiKey = 'AIzaSyDL8D-Glo1lpwoBXCMCCmAoT_5AYLSMT5c';
+
+final _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const AdaptivePlaylists();
+      },
+      routes: <RouteBase>[
+        GoRoute(
+          path: 'playlist/:id',
+          builder: (context, state) {
+            final title = state.queryParams['title']!;
+            final id = state.params['id']!;
+            return Scaffold(
+              appBar: AppBar(title: Text(title)),
+              body: PlaylistDetails(
+                playlistId: id,
+                playlistName: title,
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+void main() {
+  runApp(ChangeNotifierProvider<FlutterDevPlaylists>(
+    create: (context) => FlutterDevPlaylists(
+      flutterDevAccountId: flutterDevAccountId,
+      youTubeApiKey: youTubeApiKey,
+    ),
+    child: const PlaylistsApp(),
+  ));
+}
+
+class PlaylistsApp extends StatelessWidget {
+  const PlaylistsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Mosham | موشام Playlists',
+      theme: FlexColorScheme.light(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      darkTheme: FlexColorScheme.dark(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      themeMode: ThemeMode.dark, // Or ThemeMode.System if you'd prefer
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
+  }
+}
